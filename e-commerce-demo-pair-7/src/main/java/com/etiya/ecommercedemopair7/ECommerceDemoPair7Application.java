1d8c785a24bb0ff0582866de81ee2276616981c0
Zeynep <zeynephan4949@gmail.com>
2022-12-05 15:17:22 +0300

    created loop bean

--- e-commerce-demo-pair-7/src/main/java/com/etiya/ecommercedemopair7/ECommerceDemoPair7Application.java
+++ e-commerce-demo-pair-7/src/main/java/com/etiya/ecommercedemopair7/ECommerceDemoPair7Application.java
@@ -2,13 +2,17 @@ package com.etiya.ecommercedemopair7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ECommerceDemoPair7Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ECommerceDemoPair7Application.class, args);
 
+		ApplicationContext applicationContext = SpringApplication.run(ECommerceDemoPair7Application.class, args);
+		for (String applicationName:applicationContext.getBeanDefinitionNames()) {
+			System.out.println(applicationName);
+		}
 	}
 
 }
